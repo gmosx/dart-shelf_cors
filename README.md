9765d77f1e8142d84a81b530a0b5e95d91fd7ccf
George Moschovitis <george.moschovitis@gmail.com>
2014-12-25 10:08:02 +0200

    Created repo, basic implementation of the middleware.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Package shelf_cors
+
+A middleware to add CORS headers to shelf responses.
+
+## Usage
+
+A simple usage example:
+
+    // TODO: write the example! 
+    
+    import 'package:shelf_cors/shelf_cors.dart';
+
+    main() {
+      var awesome = new Awesome();
+    }
+
+## Features and bugs
+
+Please file feature requests and bugs at the [issue tracker][tracker].
+
+[tracker]: http://example.com/issues/replaceme
---
+++ lib/shelf_cors.dart
@@ -0,0 +1,26 @@
+library shelf.middleware.cors;
+
+import 'package:shelf/shelf.dart';
+
+/// Middleware which adds [CORS headers](https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS)
+/// to shelf responses. Also handles preflight (OPTIONS) requests.
+Middleware addCorsHeadersMiddleware({Map<String, String> corsHeaders}) {
+  if (corsHeaders == null) {
+    // By default allow access from everywhere.
+    corsHeaders = {'Access-Control-Allow-Origin': '*'};
+  }
+
+  // Handle preflight (OPTIONS) requests by just adding headers and an empty
+  // response.
+  Response handleOptionsRequest(Request request) {
+    if (request.method == 'OPTIONS') {
+      return new Response.ok(null, headers: corsHeaders);
+    } else {
+      return null;
+    }
+  }
+
+  Response addCorsHeaders(Response response) => response.change(headers: corsHeaders);
+
+  return createMiddleware(requestHandler: handleOptionsRequest, responseHandler: addCorsHeaders);
+}
